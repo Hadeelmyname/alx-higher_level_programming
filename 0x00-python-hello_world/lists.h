5195ace0571c6d9026c3a5e31148a99b294341fe
Hadeelmyname <hadeelawad62@gmail.com>
2023-11-27 16:44:51 +0000

    0-run

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,13 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s linked list
+ * @n: integer
+ * @next: points to next node
+ *
+ * Description: singly linked list node structure
+ */
+
