812d6f374995240ab21f6302eeb986c4aae837bf
Hadeelmyname <hadeelawad62@gmail.com>
2024-01-05 19:36:25 +0000

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,6 +2,9 @@
 """Defines a Rectangle class."""
 
 class Rectangle:
+    """Represent a rectangle."""
+
+def __init__(self, width=0, height=0):
     """Initialize a new Rectangle.
 
     Args:
@@ -14,20 +17,20 @@ class Rectangle:
 
     @property
     def width(self):
-        """Get/set the width of the rectangle."""
+        """Get/set the width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if not iinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >+ 0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
         @property
         def height(self):
-            """Get/set the height of the rectangle."""
+            """Get/set the height of the Rectangle."""
             return self.__height
 
         @height.setter
@@ -37,3 +40,9 @@ class Rectangle:
             if value < 0:
                 raise ValueError("height must be >= 0")
             self.__height = value
+
+            def area(self):
+                """Return the area of the Rectangle."""
+                if self.__width == 0 or self.__height == 0:
+                    return (0)
+                return ((self.__width * 2) + (self.__height * 2))
